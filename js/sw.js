0f3bf3dfd3476ad183f0fc32a3c1715beb272ea7
Kamil Wcisło <mek.xgt@gmail.com>
2022-02-15 16:27:20 +0100

    sw.js: use simple service worker

--- js/sw.js
+++ js/sw.js
@@ -11,6 +11,7 @@ Copyright 2015, 2019, 2020 Google LLC. All Rights Reserved.
  limitations under the License.
 */
 
+/* google original version
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
@@ -83,4 +84,32 @@ self.addEventListener("fetch", (event) => {
   // chance to call event.respondWith(). If no fetch handlers call
   // event.respondWith(), the request will be handled by the browser as if there
   // were no service worker involvement.
+});
+*/
+
+/* simple version */
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '.',
+  'index.html',
+  'css/style.css',
+  'js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
