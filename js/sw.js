14bfd657dd919f97b860ad6704f4a124b6b0e169
Kamil Wcisło <mek.xgt@gmail.com>
2022-02-15 16:48:35 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -90,9 +90,10 @@ self.addEventListener("fetch", (event) => {
 /* simple version */
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  'index.html',
-  'css/style.css',
-  'js/main.js'
+  '/password/'
+  '/password/index.html',
+  '/password/css/style.css',
+  '/password/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
